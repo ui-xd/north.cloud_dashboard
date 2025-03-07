4ece153c9e49d5c1558b53c3af73b439da5acbe2
Jonny <hello@proven.design>
2025-03-07 11:35:13 -0600

    Update TopNav.svelte

--- src/lib/TopNav.svelte
+++ src/lib/TopNav.svelte
@@ -1,6 +1,6 @@
-<nav class="h-16 p-2 max-w-full max-h-16 hover:bg-zenix/30 transition-all">
+<nav class="h-16 p-2 max-w-full max-h-16">
   <div
-    class="flex justify-between items-center rounded-md h-full px-4 ring-1 ring-auroa"
+    class="flex justify-between items-center rounded-md h-full px-4 ring-1 ring-auroa hover:bg-zenix/30 transition-all"
   >
     <img src="/MainLogo_V2.svg" alt="Logo" class="h-5" />
   </div>
